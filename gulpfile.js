08020fcede00b92708d55874b9f4e205a77e44ef
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-08 16:41:57 -0600

    added remainder base files

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,35 @@
 var gulp = require('gulp'),
-    stylus = require('gulp-stylus'),
+    // stylus = require('gulp-stylus'),
     concat = require('gulp-concat'),
     // uglify = require('gulp-uglify'),
     uglifycss = require('gulp-uglifycss'),
     ngAnnotate = require('gulp-ng-annotate'),
-    watcher = gulp.watch(['./main/client/src/**/*.js', './main/client/styles/*.styl'], ['default']);
+    watcher = gulp.watch(['./main/client/src/**/*.js', './main/client/styles/*.styl', './main/client/features/**/*.html'], ['default']);
 
 watcher.on('change', function( event ) {
 		console.log('File ' + event.path + ' was ' + event.type + ' at ' + new Date() + ' , running tasks...');
 });
 
-gulp.task('stylus', function() {
-	gulp.src('./main/client/styles/*.styl')
-		.pipe(stylus())
+gulp.task('styles', function() {  // will double check out styles|sass wording|npm install
+	gulp.src('./main/styles/*.css')
+		// .pipe(stylus())
 		.pipe(uglifycss())
 		.pipe(concat('styles.css'))
 		.pipe(gulp.dest('./public/styles'))
 });
 
 gulp.task('javascript', function() {
-	gulp.src('./main/client/src/**/*.js')
+	gulp.src('./main/**/*.js')
 		.pipe(ngAnnotate())
 		// .pipe(uglify())
 		.pipe(concat('all.js'))
 		.pipe(gulp.dest('./public/scripts'))
 });
 
-gulp.task('default', ['stylus', 'javascript']);
+gulp.task('html', function() {
+    gulp.src('./main/**/*.html')
+        // .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('./public/dist/html/'))
+});
+
+gulp.task('default', ['styles', 'javascript', 'html']);
