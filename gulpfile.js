1d24117c7407cc53ed2cbaa6ca4a2201ed4a6802
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-11 11:19:55 -0600

    added template html files

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,7 @@ watcher.on('change', function(event) {
 gulp.task('styles', function() { // .scss is newer file version of .sass
     gulp.src('./main/styles/*.scss')
         // .pipe(stylus())
-        .pipe(sass())
+        .pipe(styles())
         .pipe(uglifycss())
         .pipe(concat('styles.css'))
         .pipe(gulp.dest('./public/styles'))
