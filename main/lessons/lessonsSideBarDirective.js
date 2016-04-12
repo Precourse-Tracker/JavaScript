61f47113b6e71560b89b9587f61d6b2bd1366ae8
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-12 17:16:10 -0600

    moved to side nav bar and toggle on lesson topics

--- main/lessons/lessonsSideBarDirective.js
+++ main/lessons/lessonsSideBarDirective.js
@@ -6,6 +6,15 @@ angular.module('myApp')
     restrict: 'E',
     templateUrl: './html/lessons/lessonsSideBarTemplate.html',
     link: function(scope, ele, attr) {
+      $('.lesson-title').click(function() {
+        // console.log(this.parentNode);
+        $('.lesson-sections', this.parentNode).toggle('expand');
+      })
+
+      // $('.lesson-group').click(function() {
+      //   // console.log(this.parentNode);
+      //   $('.lesson-title', this.parentNode).toggle('expand');
+      // })
 
     }
   }
--- public/scripts/all.js
+++ public/scripts/all.js
@@ -34,6 +34,29 @@ angular.module('myApp', ['ui.router'])
 
 angular.module('myApp')
 
+.directive('lessonsSideBarDirective', function() {
+
+  return {
+    restrict: 'E',
+    templateUrl: './html/lessons/lessonsSideBarTemplate.html',
+    link: function(scope, ele, attr) {
+      $('.lesson-title').click(function() {
+        // console.log(this.parentNode);
+        $('.lesson-sections', this.parentNode).toggle('expand');
+      })
+
+      // $('.lesson-group').click(function() {
+      //   // console.log(this.parentNode);
+      //   $('.lesson-title', this.parentNode).toggle('expand');
+      // })
+
+    }
+  }
+
+})  // end lessonsSideBarDirective
+
+angular.module('myApp')
+
 .controller('loginController', function(){
 
   $(document).ready(function(){
@@ -72,20 +95,6 @@ angular.module('myApp')
   }
 
 }) // end loginDirective
-
-angular.module('myApp')
-
-.directive('lessonsSideBarDirective', function() {
-
-  return {
-    restrict: 'E',
-    templateUrl: './html/lessons/lessonsSideBarTemplate.html',
-    link: function(scope, ele, attr) {
-
-    }
-  }
-
-})  // end lessonsSideBarDirective
 
 angular.module('myApp')
 
