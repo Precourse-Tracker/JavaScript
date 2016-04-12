84dcdc56fc9529c55e89727707fe05a5eaec00eb
Jeff Perkins <jeff.perk87@gmail.com>
2016-04-12 17:02:33 -0600

    changed lesson schema

--- server/models/Lesson.js
+++ server/models/Lesson.js
@@ -5,7 +5,8 @@ const lessonSchema = new Schema({
   name: {type: String},
   questions: [{
     question: {type: String, required: true, index: true},
-    answer: {type: String, required: true, index: true},
+    multipleChoice: {type: Array},
+    correctAnswer: {type: String, required: true, index: true},
   }],
   score: {type: Number}
 })
