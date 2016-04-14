512fd2eedb25a7da076eb3cba1e10bcc1c82170b
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-14 11:19:25 -0600

    dashboard graph placeholders with switch views

--- main/dashboard/unitTestMenuDirective.js
+++ main/dashboard/unitTestMenuDirective.js
@@ -7,12 +7,51 @@ angular.module('myApp')
     // templateUrl: './html/dashboard/dashboardTopTemplate.html',
     link: function(scope, ele, attr) {
 
-      $('#dashboard-top').click(function() {
+      $('#dashboard-unit-tests').click(function() {
         // console.log(this);
         $('#unit-test-menu').toggle('expand');
+      })
+
+      $('#unit-test-menu').click(function() {
+        $('#unit-test-menu').toggle('expand');
+      })
+
+      // unit test graph changes for unit views and cohort compare
+      $('#js-graph').click(function() {
+        $('#js-graph-div').css('z-index', 2);
+        $('#js-graph-div').siblings().toggle().css('z-index', 0);
+      })
+
+      $('#html-graph').click(function() {
+        $('#html-graph-div').css('z-index', 2);
+        $('#html-graph-div').siblings().css('z-index', 0);
+      })
+
+      $('#css-graph').click(function() {
+        $('#css-graph-div').css('z-index', 2);
+        $('#css-graph-div').siblings().css('z-index', 0);
+      })
+
+      $('#git-graph').click(function() {
+        $('#git-graph-div').css('z-index', 2);
+        $('#git-graph-div').siblings().css('z-index', 0);
+      })
+
+      $('#cohort-compare').click(function() {
+        // console.log(this);
+        $('#cohort-graph-div').css('z-index', 2);
+        $('#cohort-graph-div').siblings().css('z-index', 0);
       })
 
     }
   }
 
 })  // end unitTestMenuDirective
+
+
+/*
+
+      $('#profile-wrapper').click(function() {
+        profileMenu.toggle('expand')
+      })
+*/
