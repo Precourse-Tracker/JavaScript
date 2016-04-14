52e858a0da90eb5f48e5943d64af2f826df843d2
Jeff Perkins <jeff.perk87@gmail.com>
2016-04-14 16:23:47 -0600

    gulp commit

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -32,17 +32,28 @@ angular.module('myApp', ['ui.router'])
 
 }]) // end config
 
-angular.module('myApp').service('WebWorkerService', ["assessmentService", "$http", "$q", function(assessmentService, $http, $q) {
-  
-}])
-
 angular.module('myApp').controller('assessmentController', ["$scope", "assessmentService", function($scope, assessmentService) {
-  
+
+  $scope.getAssessment = () => {
+    assessmentService.getLesson().then((assessment) => {
+      $scope.assessment = assessment;
+    })
+  }
 }])
 
-angular.module('myApp').service('assessmentService', ["$q", "$http", "WebWorkerService", function($q, $http, WebWorkerService) {
+angular.module('myApp').service('assessmentService', ["$q", "$http", function($q, $http) {
 
+
+    this.getAssessment = () => {
+        return $http({
+            method: 'GET',
+            url: '/api/assessment/js'
+        }).then((response) => {
+            return response;
+        })
+    }
 }])
+
 
 angular.module('myApp')
 
