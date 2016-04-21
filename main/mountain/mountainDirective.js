a39f5a3a6944715c8753d998353c57e261fd8030
tyson_taylor <tyson.james.taylor@gmail.com>
2016-04-20 22:09:59 -0600

    progress on trees

--- main/mountain/mountainDirective.js
+++ main/mountain/mountainDirective.js
@@ -15,97 +15,65 @@ angular.module( 'myApp' )
             x: posX,
           }
         }
+        const findY = function ( x ) {
+          console.log( 'x from inside findY', x );
+          var yMax
+          if ( x < ( xMax / 2 ) ) {
+            yMax = ( x + 7 );
+          } else {
+            yMax = ( xMax - x ) + 4;
+          }
+          return yMax;
+        }
 
         scope.spawnTrees = function ( number ) {
           console.log( 'spawnTrees was called' );
           for ( var i = 0; i < number; i++ ) {
+            console.log( 'spawnTrees was looped: ' + [ i + 1 ] + ' times' );
             var aCoords = randomLocation( -3, 33 /*, 5, 22*/ );
             var bCoords = randomLocation( -3, 33 /*, 5, 22*/ );
             var x = aCoords.x;
             var y = randomLocation( 5, findY( aCoords.x ) ).x;
 
-            function findY( x ) {
-              console.log( 'x from inside findY', x );
-              var yMax
-              if ( x < ( xMax / 2 ) ) {
-                yMax = ( x + 7 );
-              } else {
-                yMax = ( xMax - x ) + 4;
-              }
-              return yMax;
-            }
-            console.log( 'spawnTrees was looped: ' + [ i + 1 ] + ' times' );
             var node = document.getElementById( 'mtn-wrapper' );
-            var newDiv = document.createElement( 'div' );
+            var myTrees = document.getElementById( 'my-icon' );
             var i1 = document.createElement( 'i' );
             var i2 = document.createElement( 'i' );
             var i3 = document.createElement( 'i' );
             var i4 = document.createElement( 'i' );
-            // newDiv.style.width = '2.4em';
-            // newDiv.style.height = '2em';
-            // newDiv.innerHTML = 'Hello';
-            // newDiv.style.position = 'absolute';
-            // newDiv.style.left = x + 'em';
-            // newDiv.style.bottom = y + 'em';
-            // newDiv.style.background = 'red';
-            i1.style.width = '2em';
+
             i1.style.border = '0.55em solid transparent';
-            i1.innerHTML = 'Hello';
             i1.style.margin = '-0.55em 0 0 0';
-            i1.style.width = '';
-            i1.style.width = '';
-            i1.style.width = '';
-            i1.style.width = '';
-            i1.style.width = '';
+            i1.style.borderBottom = '0.55em solid green';
+            i1.style.left = (x + 1.1) + 'em';
+            i1.style.top = (y + 0.78) + 'em';
 
-/*.my-icon > i {
-    : ;
-    : ;
-    border-bottom: 0.55em solid green;
-    left: 1.045em;
-    top: 0.78em;
-}
+            i2.style.border = '0.55em solid transparent';
+            i2.style.marginTop = '-0.55em';
+            i2.style.borderBottom = '0.55em solid green';
+            i2.style.left = (x - 0.2) + 'em';
+            i2.style.top = (y - 0.47) + 'em';
 
-.my-icon > i+i {
+            i3.style.border = '0.39em solid transparent';
+            i3.style.margin = '-0.39em 0 0 0';
+            i3.style.borderBottom = '0.39em solid green';
+            i3.style.left = (x + 1.185), 'em';
+            i3.style.top = (y - 1.1), 'em';
 
-    border: 0.55em solid transparent;
-    margin-top: -0.55em;
-    border-bottom: 0.55em solid green;
-    left: -0.2em;
-    top: 0.47em;
-}
+            i4.style.border = 'none';
+            i4.style.width = '.15em';
+            i4.style.height = '0.31em';
+            i4.style.backgroundColor = '#522200';
+            i4.style.left = (x + 0.53)'em';
+            i4.style.top = (y - 0.2)'em';
 
-.my-icon > i+i+i {
-    position: relative;
-    display: inline-block;
-    width: 0;
-    height: 0;
-    line-height: 0;
-    border: 0.39473684210526316em solid transparent;
-    margin-top: -0.39473684210526316em;
-    border-bottom: 0.39473684210526316em solid green;
-    left: 1.185em;
-    top: -1.1em;
-}
-
-.my-icon > i+i+i+i {
-    position: relative;
-    display: inline-block;
-    border: 0px;
-    border-style: none;
-    width: 0.15em;
-    height: 0.31em;
-    background-color: #522200;
-    left: 0.53em;
-    top: -0.2em;
-}*/
-
-
-            node.appendChild( newDiv );
+            // node.appendChild( newDiv );
             node.appendChild( i1 );
-
+            node.appendChild( i2 );
+            node.appendChild( i3 );
+            node.appendChild( i4 );
             console.log( 'x: ' + x, 'y: ', y );
-            console.log( newDiv.style );
+            // console.log( newDiv.style );
 
             var currentDiv = document.getElementsByClassName( 'trees' )[ 0 ];
             // trees.insertBefore( newDiv, currentDiv );
@@ -142,3 +110,16 @@ angular.module( 'myApp' )
 // var Qy = Math.floor( ( ( ay1 * vy1 ) + ( ay2 * vy2 ) ) / 10 );
 // console.log( 'Qx: ', Qx );
 // console.log( 'Qy: ', Qy );
+
+
+
+// div  \\
+// var newDiv = document.createElement( 'div' );
+// newDiv.style.width = '2.4em';
+// newDiv.style.height = '2em';
+// newDiv.innerHTML = 'Hello';
+// newDiv.style.position = 'absolute';
+// newDiv.style.left = x + 'em';
+// newDiv.style.bottom = y + 'em';
+// newDiv.style.background = 'red';
+// i1.style.width = '2em';
--- public/scripts/all.js
+++ public/scripts/all.js
@@ -34,6 +34,40 @@ angular.module('myApp', ['ui.router'])
 
 angular.module('myApp')
 
+.controller('assessmentController', ["$scope", "assessmentService", function($scope, assessmentService) {
+
+  $scope.getAssessment = () => {
+    assessmentService.getLesson().then((assessment) => {
+      $scope.assessment = assessment;
+    })
+  }
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/chrome");
+editor.getSession().setMode("ace/mode/javascript");
+
+var editor_1 = ace.edit("editor_1");
+editor_1.setTheme("ace/theme/chrome");
+editor_1.getSession().setMode("ace/mode/javascript");
+
+}])
+
+angular.module('myApp').service('assessmentService', ["$q", "$http", function($q, $http) {
+
+
+    this.getAssessment = () => {
+        return $http({
+            method: 'GET',
+            url: '/api/assessment/js'
+        }).then((response) => {
+            return response;
+        })
+    }
+}])
+
+
+angular.module('myApp')
+
 .directive('unitTestMenuDirective', function() {
 
   return {
@@ -92,40 +126,6 @@ angular.module('myApp')
 
 angular.module('myApp')
 
-.controller('assessmentController', ["$scope", "assessmentService", function($scope, assessmentService) {
-
-  $scope.getAssessment = () => {
-    assessmentService.getLesson().then((assessment) => {
-      $scope.assessment = assessment;
-    })
-  }
-
-var editor = ace.edit("editor");
-editor.setTheme("ace/theme/chrome");
-editor.getSession().setMode("ace/mode/javascript");
-
-var editor_1 = ace.edit("editor_1");
-editor_1.setTheme("ace/theme/chrome");
-editor_1.getSession().setMode("ace/mode/javascript");
-
-}])
-
-angular.module('myApp').service('assessmentService', ["$q", "$http", function($q, $http) {
-
-
-    this.getAssessment = () => {
-        return $http({
-            method: 'GET',
-            url: '/api/assessment/js'
-        }).then((response) => {
-            return response;
-        })
-    }
-}])
-
-
-angular.module('myApp')
-
 .directive('lessonsSideBarDirective', function() {
 
   return {
@@ -180,193 +180,6 @@ angular.module('myApp')
   }
 
 })
-
-angular.module( 'myApp' )
-  .controller( 'mountainController', [ '$scope', 'loginService', function ( $scope, loginService ) {
-
-    $scope.logoutUser = function () {
-      loginService.logoutUser();
-    };
-
-
-} ] );
-
-angular.module( 'myApp' )
-  .directive( 'mountainDirective', function () {
-    var dirDefinition = {
-      restrict: 'E',
-      templateUrl: './html/mountain/mountainTemplate.html',
-      controller: 'mountainController',
-      link: function ( scope, element, attrs, controller, transcludeFn, animate ) {
-        const xMin = -3;
-        const xMax = 33;
-
-        const randomLocation = function ( xMin, xMax /*, yMin, yMax*/ ) {
-          console.log( 'randomLocation is logging' );
-          var posX = Math.floor( Math.random() * ( xMax - xMin + 1 ) ) + xMin;
-          return {
-            x: posX,
-          }
-        }
-
-        scope.spawnTrees = function ( number ) {
-          console.log( 'spawnTrees was called' );
-          for ( var i = 0; i < number; i++ ) {
-            var aCoords = randomLocation( -3, 33 /*, 5, 22*/ );
-            var bCoords = randomLocation( -3, 33 /*, 5, 22*/ );
-            var x = aCoords.x;
-            var y = randomLocation( 5, findY( aCoords.x ) ).x;
-
-            function findY( x ) {
-              console.log( 'x from inside findY', x );
-              var yMax
-              if ( x < ( xMax / 2 ) ) {
-                yMax = ( x + 7 );
-              } else {
-                yMax = ( xMax - x ) + 4;
-              }
-              return yMax;
-            }
-            console.log( 'spawnTrees was looped: ' + [ i + 1 ] + ' times' );
-            var node = document.getElementById( 'mtn-wrapper' );
-            var newDiv = document.createElement( 'div' );
-            var i1 = document.createElement( 'i' );
-            var i2 = document.createElement( 'i' );
-            var i3 = document.createElement( 'i' );
-            var i4 = document.createElement( 'i' );
-            // newDiv.style.width = '2.4em';
-            // newDiv.style.height = '2em';
-            // newDiv.innerHTML = 'Hello';
-            // newDiv.style.position = 'absolute';
-            // newDiv.style.left = x + 'em';
-            // newDiv.style.bottom = y + 'em';
-            // newDiv.style.background = 'red';
-            i1.style.width = '2em';
-            i1.style.border = '0.55em solid transparent';
-            i1.innerHTML = 'Hello';
-            i1.style.margin = '-0.55em 0 0 0';
-            i1.style.width = '';
-            i1.style.width = '';
-            i1.style.width = '';
-            i1.style.width = '';
-            i1.style.width = '';
-
-/*.my-icon > i {
-    : ;
-    : ;
-    border-bottom: 0.55em solid green;
-    left: 1.045em;
-    top: 0.78em;
-}
-
-.my-icon > i+i {
-
-    border: 0.55em solid transparent;
-    margin-top: -0.55em;
-    border-bottom: 0.55em solid green;
-    left: -0.2em;
-    top: 0.47em;
-}
-
-.my-icon > i+i+i {
-    position: relative;
-    display: inline-block;
-    width: 0;
-    height: 0;
-    line-height: 0;
-    border: 0.39473684210526316em solid transparent;
-    margin-top: -0.39473684210526316em;
-    border-bottom: 0.39473684210526316em solid green;
-    left: 1.185em;
-    top: -1.1em;
-}
-
-.my-icon > i+i+i+i {
-    position: relative;
-    display: inline-block;
-    border: 0px;
-    border-style: none;
-    width: 0.15em;
-    height: 0.31em;
-    background-color: #522200;
-    left: 0.53em;
-    top: -0.2em;
-}*/
-
-
-            node.appendChild( newDiv );
-            node.appendChild( i1 );
-
-            console.log( 'x: ' + x, 'y: ', y );
-            console.log( newDiv.style );
-
-            var currentDiv = document.getElementsByClassName( 'trees' )[ 0 ];
-            // trees.insertBefore( newDiv, currentDiv );
-          }
-        }
-      }
-    }
-    return dirDefinition;
-
-  } ) // end mountainDirective
-
-// various attempts at constraining trees \\
-// var base = Math.abs( aCoords.x - bCoords.x );
-// var height = Math.abs( aCoords.y - bCoords.y );
-//
-// console.log( 'base: ', base, 'height: ', height );
-// var aLength = 3;
-// var bLength = 4;
-// var hypLength = Math.sqrt( aLength * aLength + bLength * bLength );
-// // console.log('aCoordsX ', aCoords.x, 'aCoordsY: ', aCoords.y);
-
-
-
-// better randomization \\
-// var v1 = Math.floor( Math.random() * 10 );
-// var v2 = Math.floor( Math.random() * 10 );
-// var a1 = Math.floor( Math.random() * 10 );
-// var a2 = Math.floor( Math.random() * 10 );
-// var vy1 = Math.floor( Math.random() * 10 );
-// var vy2 = Math.floor( Math.random() * 10 );
-// var ay1 = Math.floor( Math.random() * 10 );
-// var ay2 = Math.floor( Math.random() * 10 );
-// var Qx = Math.floor( ( ( a1 * v1 ) + ( a2 * v2 ) ) / 10 );
-// var Qy = Math.floor( ( ( ay1 * vy1 ) + ( ay2 * vy2 ) ) / 10 );
-// console.log( 'Qx: ', Qx );
-// console.log( 'Qy: ', Qy );
-
-angular.module('myApp')
-
-.controller('navigationController', ["$scope", "loginService", function($scope, loginService) {
-
-  $scope.logoutUser = function() {
-    loginService.logoutUser();
-  };
-
-}])
-
-angular.module('myApp')
-
-.directive('navigationDirective', function() {
-
-  return {
-    restrict: 'E',
-    templateUrl: './html/navigation/navigationTemplate.html',
-    link: function(scope, ele, attr) {
-      let profileMenu = $('#menu-navigation');
-
-      $('#profile-wrapper').click(function() {
-        profileMenu.toggle('expand')
-      })
-
-      profileMenu.click(function() {
-        profileMenu.toggle('expand');
-      })
-    }
-  }
-
-}) // end navigationDirective
 
 angular.module('myApp')
 .controller('loginController', ["$scope", "loginService", function($scope, loginService){
@@ -459,3 +272,171 @@ angular.module("myApp")
     });
   };
 }]);
+
+angular.module( 'myApp' )
+  .controller( 'mountainController', [ '$scope', 'loginService', function ( $scope, loginService ) {
+
+    $scope.logoutUser = function () {
+      loginService.logoutUser();
+    };
+
+
+} ] );
+
+angular.module( 'myApp' )
+  .directive( 'mountainDirective', function () {
+    var dirDefinition = {
+      restrict: 'E',
+      templateUrl: './html/mountain/mountainTemplate.html',
+      controller: 'mountainController',
+      link: function ( scope, element, attrs, controller, transcludeFn, animate ) {
+        const xMin = -3;
+        const xMax = 33;
+
+        const randomLocation = function ( xMin, xMax /*, yMin, yMax*/ ) {
+          console.log( 'randomLocation is logging' );
+          var posX = Math.floor( Math.random() * ( xMax - xMin + 1 ) ) + xMin;
+          return {
+            x: posX,
+          }
+        }
+        const findY = function ( x ) {
+          console.log( 'x from inside findY', x );
+          var yMax
+          if ( x < ( xMax / 2 ) ) {
+            yMax = ( x + 7 );
+          } else {
+            yMax = ( xMax - x ) + 4;
+          }
+          return yMax;
+        }
+
+        scope.spawnTrees = function ( number ) {
+          console.log( 'spawnTrees was called' );
+          for ( var i = 0; i < number; i++ ) {
+            console.log( 'spawnTrees was looped: ' + [ i + 1 ] + ' times' );
+            var aCoords = randomLocation( -3, 33 /*, 5, 22*/ );
+            var bCoords = randomLocation( -3, 33 /*, 5, 22*/ );
+            var x = aCoords.x;
+            var y = randomLocation( 5, findY( aCoords.x ) ).x;
+
+            var node = document.getElementById( 'mtn-wrapper' );
+            var myTrees = document.getElementById( 'my-icon' );
+            var i1 = document.createElement( 'i' );
+            var i2 = document.createElement( 'i' );
+            var i3 = document.createElement( 'i' );
+            var i4 = document.createElement( 'i' );
+
+            i1.style.border = '0.55em solid transparent';
+            i1.style.margin = '-0.55em 0 0 0';
+            i1.style.borderBottom = '0.55em solid green';
+            i1.style.left = (x + 1.1) + 'em';
+            i1.style.top = (y + 0.78) + 'em';
+
+            i2.style.border = '0.55em solid transparent';
+            i2.style.marginTop = '-0.55em';
+            i2.style.borderBottom = '0.55em solid green';
+            i2.style.left = (x - 0.2) + 'em';
+            i2.style.top = (y - 0.47) + 'em';
+
+            i3.style.border = '0.39em solid transparent';
+            i3.style.margin = '-0.39em 0 0 0';
+            i3.style.borderBottom = '0.39em solid green';
+            i3.style.left = (x + 1.185),'em';
+            i3.style.top = (y - 1.1),'em';
+
+            i4.style.border = 'none';
+            i4.style.width = '.15em';
+            i4.style.height = '0.31em';
+            i4.style.backgroundColor = '#522200';
+            i4.style.left = '0.53em';
+            i4.style.top = '-0.2em';
+
+            // node.appendChild( newDiv );
+            node.appendChild( i1 );
+            node.appendChild( i2 );
+            node.appendChild( i3 );
+            node.appendChild( i4 );
+            console.log( 'x: ' + x, 'y: ', y );
+            // console.log( newDiv.style );
+
+            var currentDiv = document.getElementsByClassName( 'trees' )[ 0 ];
+            // trees.insertBefore( newDiv, currentDiv );
+          }
+        }
+      }
+    }
+    return dirDefinition;
+
+  } ) // end mountainDirective
+
+// various attempts at constraining trees \\
+// var base = Math.abs( aCoords.x - bCoords.x );
+// var height = Math.abs( aCoords.y - bCoords.y );
+//
+// console.log( 'base: ', base, 'height: ', height );
+// var aLength = 3;
+// var bLength = 4;
+// var hypLength = Math.sqrt( aLength * aLength + bLength * bLength );
+// // console.log('aCoordsX ', aCoords.x, 'aCoordsY: ', aCoords.y);
+
+
+
+// better randomization \\
+// var v1 = Math.floor( Math.random() * 10 );
+// var v2 = Math.floor( Math.random() * 10 );
+// var a1 = Math.floor( Math.random() * 10 );
+// var a2 = Math.floor( Math.random() * 10 );
+// var vy1 = Math.floor( Math.random() * 10 );
+// var vy2 = Math.floor( Math.random() * 10 );
+// var ay1 = Math.floor( Math.random() * 10 );
+// var ay2 = Math.floor( Math.random() * 10 );
+// var Qx = Math.floor( ( ( a1 * v1 ) + ( a2 * v2 ) ) / 10 );
+// var Qy = Math.floor( ( ( ay1 * vy1 ) + ( ay2 * vy2 ) ) / 10 );
+// console.log( 'Qx: ', Qx );
+// console.log( 'Qy: ', Qy );
+
+
+
+// div  \\
+// var newDiv = document.createElement( 'div' );
+// newDiv.style.width = '2.4em';
+// newDiv.style.height = '2em';
+// newDiv.innerHTML = 'Hello';
+// newDiv.style.position = 'absolute';
+// newDiv.style.left = x + 'em';
+// newDiv.style.bottom = y + 'em';
+// newDiv.style.background = 'red';
+// i1.style.width = '2em';
+
+angular.module('myApp')
+
+.controller('navigationController', ["$scope", "loginService", function($scope, loginService) {
+
+  $scope.logoutUser = function() {
+    loginService.logoutUser();
+  };
+
+}])
+
+angular.module('myApp')
+
+.directive('navigationDirective', function() {
+
+  return {
+    restrict: 'E',
+    templateUrl: './html/navigation/navigationTemplate.html',
+    link: function(scope, ele, attr) {
+      let profileMenu = $('#menu-navigation');
+
+      $('#profile-wrapper').click(function() {
+        profileMenu.toggle('expand')
+      })
+
+      profileMenu.click(function() {
+        profileMenu.toggle('expand');
+      })
+    }
+  }
+
+}) // end navigationDirective
