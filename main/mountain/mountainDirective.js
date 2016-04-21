43c188ac2af50e467f660729f9c74feff5ee469c
tyson_taylor <tyson.james.taylor@gmail.com>
2016-04-21 16:42:55 -0600

    trees now confined within placeholder pyramid/Triangle

--- main/mountain/mountainDirective.js
+++ main/mountain/mountainDirective.js
@@ -36,7 +36,7 @@ angular.module( 'myApp' )
             var x = aCoords;
             var y = randomMinMax( 5, findY( aCoords ) );
 
-            var node = document.getElementById( 'mtn-wrapper' );
+            var node = document.getElementById( 'home-top' );
             var myTrees = document.getElementById( 'my-icon' );
 
             var i1 = document.createElement( 'i' );
@@ -53,22 +53,22 @@ angular.module( 'myApp' )
             i1.style.zIndex = (newZ);
             i1.style.border = '0.39em solid transparent';
             i1.style.borderBottom = '0.39em solid ' + newGrn;
-            i1.style.left = ( x - 0.2 ) + 'em';
-            i1.style.bottom = ( y + 1.1 ) + 'em';
+            i1.style.left = ( x - 16.2 ) + 'em';
+            i1.style.bottom = ( y -24.9 ) + 'em';
 
             // mid branches \\
             i2.style.zIndex = (newZ);
             i2.style.border = '0.55em solid transparent';
             i2.style.borderBottom = '0.55em solid ' + newGrn;
-            i2.style.left = ( x - 0.4 ) + 'em';
-            i2.style.bottom = ( y + 0.75 ) + 'em';
+            i2.style.left = ( x - 16.4 ) + 'em';
+            i2.style.bottom = ( y -24.25 ) + 'em';
 
             // base branches \\
             i3.style.zIndex = (newZ);
             i3.style.border = '0.55em solid transparent';
             i3.style.borderBottom = '0.75em solid ' + newGrn;
-            i3.style.left = ( x - 0.4 ) + 'em';
-            i3.style.bottom = ( y + 0.3 ) + 'em';
+            i3.style.left = ( x - 16.4 ) + 'em';
+            i3.style.bottom = ( y -24.7 ) + 'em';
 
             // trunk bottom \\
             i4.style.zIndex = (trunkZ);
@@ -77,8 +77,8 @@ angular.module( 'myApp' )
             i4.style.height = '0.31em';
             i4.style.marginBottom = '0em';
             i4.style.backgroundColor = '#522200';
-            i4.style.left = ( x ) + 'em';
-            i4.style.bottom = ( y ) + 'em';
+            i4.style.left = ( x - 16 ) + 'em';
+            i4.style.bottom = ( y - 25 ) + 'em';
 
             node.appendChild( i1 );
             node.appendChild( i2 );
