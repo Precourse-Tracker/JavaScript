982867d1fecb961b2db65280c118b51c546f7485
Jeff Perkins <jeff.perk87@gmail.com>
2016-04-24 12:16:11 -0600

    gulp commit

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -34,7 +34,6 @@ angular.module('myApp', ['ui.router', 'ui.ace', 'ngWebworker'])
 
 angular.module('myApp')
 
-
    .directive('bars', function () {
       return {
          restrict: 'EA',
@@ -523,64 +522,6 @@ angular.module('myApp').service('workerService', ["Webworker", function(Webworke
 
 angular.module('myApp')
 
-.directive('unitTestMenuDirective', function() {
-
-  return {
-    restrict: 'AE',
-    // templateUrl: './html/dashboard/dashboardTopTemplate.html',
-    link: function(scope, ele, attr) {
-
-      $('#dashboard-unit-tests').click(function() {
-        // console.log(this);
-        $('#unit-test-menu').toggle('expand');
-      })
-
-      $('#unit-test-menu').click(function() {
-        $('#unit-test-menu').toggle('expand');
-      })
-
-      // unit test graph changes for unit views and cohort compare
-      $('#js-graph').click(function() {
-        $('#js-graph-div').css('z-index', 2);
-        $('#js-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#html-graph').click(function() {
-        $('#html-graph-div').css('z-index', 2);
-        $('#html-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#css-graph').click(function() {
-        $('#css-graph-div').css('z-index', 2);
-        $('#css-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#git-graph').click(function() {
-        $('#git-graph-div').css('z-index', 2);
-        $('#git-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#cohort-compare').click(function() {
-        // console.log(this);
-        $('#cohort-graph-div').css('z-index', 2);
-        $('#cohort-graph-div').siblings().css('z-index', 0);
-      })
-
-    }
-  }
-
-})  // end unitTestMenuDirective
-
-
-/*
-
-      $('#profile-wrapper').click(function() {
-        profileMenu.toggle('expand')
-      })
-*/
-
-angular.module('myApp')
-
 .controller('lessonTestsController', ["$scope", function($scope) {
 
   // $scope.test = 'test on ctrl';
@@ -855,7 +796,7 @@ angular.module('myApp')
     templateUrl: './html/login/loginTemplate.html',
     link: function(scope, ele, attr) {
 
-
+  
 
     }
   }
@@ -1487,6 +1428,6 @@ describe("true", function() {
 
 
   it("should contain users code and question object properties", function() {
-
+    
   });
 });
