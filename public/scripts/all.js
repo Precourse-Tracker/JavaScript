5740694565b656ccf56d8be631ba92a2cb1d8d3a
Jeff Perkins <jeff.perk87@gmail.com>
2016-04-24 11:54:19 -0600

    gulp commit

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -34,64 +34,6 @@ angular.module('myApp', ['ui.router', 'ui.ace', 'ngWebworker'])
 
 angular.module('myApp')
 
-.directive('unitTestMenuDirective', function() {
-
-  return {
-    restrict: 'AE',
-    // templateUrl: './html/dashboard/dashboardTopTemplate.html',
-    link: function(scope, ele, attr) {
-
-      $('#dashboard-unit-tests').click(function() {
-        // console.log(this);
-        $('#unit-test-menu').toggle('expand');
-      })
-
-      $('#unit-test-menu').click(function() {
-        $('#unit-test-menu').toggle('expand');
-      })
-
-      // unit test graph changes for unit views and cohort compare
-      $('#js-graph').click(function() {
-        $('#js-graph-div').css('z-index', 2);
-        $('#js-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#html-graph').click(function() {
-        $('#html-graph-div').css('z-index', 2);
-        $('#html-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#css-graph').click(function() {
-        $('#css-graph-div').css('z-index', 2);
-        $('#css-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#git-graph').click(function() {
-        $('#git-graph-div').css('z-index', 2);
-        $('#git-graph-div').siblings().css('z-index', 0);
-      })
-
-      $('#cohort-compare').click(function() {
-        // console.log(this);
-        $('#cohort-graph-div').css('z-index', 2);
-        $('#cohort-graph-div').siblings().css('z-index', 0);
-      })
-
-    }
-  }
-
-})  // end unitTestMenuDirective
-
-
-/*
-
-      $('#profile-wrapper').click(function() {
-        profileMenu.toggle('expand')
-      })
-*/
-
-angular.module('myApp')
-
 .controller('assessmentController', ["$scope", "assessmentService", "workerService", function($scope, assessmentService, workerService) {
 
   assessmentService.getAssessment().then(function(response) {
@@ -193,6 +135,64 @@ angular.module('myApp').service('workerService', ["Webworker", function(Webworke
     return result;
   }
 }])
+
+angular.module('myApp')
+
+.directive('unitTestMenuDirective', function() {
+
+  return {
+    restrict: 'AE',
+    // templateUrl: './html/dashboard/dashboardTopTemplate.html',
+    link: function(scope, ele, attr) {
+
+      $('#dashboard-unit-tests').click(function() {
+        // console.log(this);
+        $('#unit-test-menu').toggle('expand');
+      })
+
+      $('#unit-test-menu').click(function() {
+        $('#unit-test-menu').toggle('expand');
+      })
+
+      // unit test graph changes for unit views and cohort compare
+      $('#js-graph').click(function() {
+        $('#js-graph-div').css('z-index', 2);
+        $('#js-graph-div').siblings().css('z-index', 0);
+      })
+
+      $('#html-graph').click(function() {
+        $('#html-graph-div').css('z-index', 2);
+        $('#html-graph-div').siblings().css('z-index', 0);
+      })
+
+      $('#css-graph').click(function() {
+        $('#css-graph-div').css('z-index', 2);
+        $('#css-graph-div').siblings().css('z-index', 0);
+      })
+
+      $('#git-graph').click(function() {
+        $('#git-graph-div').css('z-index', 2);
+        $('#git-graph-div').siblings().css('z-index', 0);
+      })
+
+      $('#cohort-compare').click(function() {
+        // console.log(this);
+        $('#cohort-graph-div').css('z-index', 2);
+        $('#cohort-graph-div').siblings().css('z-index', 0);
+      })
+
+    }
+  }
+
+})  // end unitTestMenuDirective
+
+
+/*
+
+      $('#profile-wrapper').click(function() {
+        profileMenu.toggle('expand')
+      })
+*/
 
 angular.module('myApp')
 
@@ -311,98 +311,6 @@ angular.module('myApp')
 // }) // end lessonTestsService
 
 angular.module('myApp')
-.controller('loginController', ["$scope", "loginService", function($scope, loginService){
-
-  $scope.createUser = function(newUser) {
-    loginService.newUser(newUser).then(function() {
-      $scope.newUser.username = '';
-      $scope.newUser.email = '';
-      $scope.newUser.password = '';
-      alert('You have successfully signed up. Please log in');
-    })
-  };
-  $scope.userLogin = function(user) {
-    loginService.userLogin(user);
-  };
-
-// jquery animations
-  $(document).ready(function(){
-  $('#goRight').on('click', function(){
-    $('#slideBox').animate({
-      'marginLeft' : '0'
-    })
-    $('.topLayer').animate({
-      'marginLeft' : '100%'
-    })
-  })
-  $('#goLeft').on('click', function(){
-    $('#slideBox').animate({
-      'marginLeft' : '50%'
-    })
-    $('.topLayer').animate({
-      'marginLeft': '0'
-    })
-  })
-})
-}])
-
-angular.module('myApp')
-
-.directive('loginDirective', function() {
-
-  return {
-    restrict: 'E',
-    templateUrl: './html/login/loginTemplate.html',
-    link: function(scope, ele, attr) {
-
-  
-
-    }
-  }
-
-}) // end loginDirective
-
-angular.module("myApp")
-.service('loginService', ["$q", "$http", "$state", function($q, $http, $state) {
-
-  this.userLogin = function(user) {
-    return $http({
-      method: 'POST',
-      data: user,
-      url: '/api/login'
-    }).success(function() {
-      $state.go('home');
-    });
-  };
-
-  this.logoutUser = function() {
-    return $http({
-      method: 'GET',
-      url: '/logout'
-    }).success(function() {
-       $state.go('login');
-    });
-  };
-
-  this.newUser = function(newUser) {
-    return $http({
-      method: 'POST',
-      data: newUser,
-      url: '/api/signup'
-    }).success(function() {
-      return;
-    });
-  };
-
-  this.getProfile = function() {
-    return $http({
-      method: 'GET',
-      url: '/user/current'
-    });
-  };
-}]);
-
-angular.module('myApp')
 
 .controller('lessonsContentController', ["$scope", "lessonsContentService", function($scope, lessonsContentService) {
   $scope.userAnswerArray = [];
@@ -516,6 +424,98 @@ angular.module('myApp')
   }
 
 }])  // end lessonsSideBarDirective
+
+angular.module('myApp')
+.controller('loginController', ["$scope", "loginService", function($scope, loginService){
+
+  $scope.createUser = function(newUser) {
+    loginService.newUser(newUser).then(function() {
+      $scope.newUser.username = '';
+      $scope.newUser.email = '';
+      $scope.newUser.password = '';
+      alert('You have successfully signed up. Please log in');
+    })
+  };
+  $scope.userLogin = function(user) {
+    loginService.userLogin(user);
+  };
+
+// jquery animations
+  $(document).ready(function(){
+  $('#goRight').on('click', function(){
+    $('#slideBox').animate({
+      'marginLeft' : '0'
+    })
+    $('.topLayer').animate({
+      'marginLeft' : '100%'
+    })
+  })
+  $('#goLeft').on('click', function(){
+    $('#slideBox').animate({
+      'marginLeft' : '50%'
+    })
+    $('.topLayer').animate({
+      'marginLeft': '0'
+    })
+  })
+})
+}])
+
+angular.module('myApp')
+
+.directive('loginDirective', function() {
+
+  return {
+    restrict: 'E',
+    templateUrl: './html/login/loginTemplate.html',
+    link: function(scope, ele, attr) {
+
+  
+
+    }
+  }
+
+}) // end loginDirective
+
+angular.module("myApp")
+.service('loginService', ["$q", "$http", "$state", function($q, $http, $state) {
+
+  this.userLogin = function(user) {
+    return $http({
+      method: 'POST',
+      data: user,
+      url: '/api/login'
+    }).success(function() {
+      $state.go('home');
+    });
+  };
+
+  this.logoutUser = function() {
+    return $http({
+      method: 'GET',
+      url: '/logout'
+    }).success(function() {
+       $state.go('login');
+    });
+  };
+
+  this.newUser = function(newUser) {
+    return $http({
+      method: 'POST',
+      data: newUser,
+      url: '/api/signup'
+    }).success(function() {
+      return;
+    });
+  };
+
+  this.getProfile = function() {
+    return $http({
+      method: 'GET',
+      url: '/user/current'
+    });
+  };
+}]);
 
 angular.module('myApp')
 
