46db050d1e1fbd9610729c5765249ec4018f4408
Laarni Astrid <laarni.astrid@gmail.com>
2016-04-27 16:47:03 -0600

    removed lessontest files (not needed anymore), updated ui view change when going to test

--- main/lessonTests/lessonFiles/js-lesson-functions.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp')
-
-.directive('jsFunctionsDirective', function() {
-  return {
-    restrict: 'A',
-    controller: 'lessonTestsController',
-    templateUrl: './html/lessonTests/lessonFiles/js-lesson-functions.html'
-  }
-}) // end varsTestDirective
