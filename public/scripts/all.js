8ffb5a27fbd4603a9e2ac74bbfeef36eb0c4a0e3
Jeff Perkins <jeff.perk87@gmail.com>
2016-04-27 19:25:11 -0600

    gulp commit

--- public/scripts/all.js
+++ public/scripts/all.js
@@ -523,52 +523,30 @@ angular.module('myApp')
 
 angular.module('myApp')
 
-.controller('lessonsController', ["$scope", function($scope) {
+.controller('lessonTestsController', ["$scope", function($scope) {
 
-  
 
-}])  // end lessonsController
+
+}])  // end lessonTestsController
 
 angular.module('myApp')
 
-.directive('lessonsSideBarDirective', ["$state", "$http", "$q", "lessonsContentService", function($state, $http, $q, lessonsContentService) {
+.directive('lessonTestsDirective', function() {
 
   return {
-    restrict: 'E',
-    controller: 'lessonsContentController',
-    templateUrl: './html/lessons/lessonsSideBarTemplate.html',
+    restrict: 'A',
     link: function(scope, ele, attr) {
-      $('.lesson-title').click(function() {
-        let that = this;
-        
-        if ($state.name !== 'lessons') {
-          $state.go('lessons')
-        }
-        $('.lesson-sections', that.parentNode).toggle('expand');
-        $('.lesson-tests-wrapper').css('display', 'none');
+      $('.reset-test').click(function() {
+        $('.final-score').css('display', 'none');
       })
-
-      let testNavigation = () => {
-        $('.lesson-tests-wrapper').css('display', 'block');
-        $('html, body').animate({ scrollTop: 0 }, 300);
-      }
-
-      $('.lesson-test').click(function() {
-        let lessonId = lessonsContentService.getTempId();
-        if ($state.name !== 'lessonTests') {
-          $state.go('lessonTests');
-          setTimeout(() => {
-            testNavigation();
-          }, 100);
-        } else {
-          testNavigation();
-        }
-      }) // end lesson-test click
-
-    } // end of directive link
+      $('.lessons').click(function(){
+        $('.final-score').css('display', 'none');
+      })
+    }
   }
 
-}])  // end lessonsSideBarDirective
+})  // end lessonTestsDirective
+
 
 angular.module('myApp')
 
@@ -715,30 +693,52 @@ angular.module('myApp')
 
 angular.module('myApp')
 
-.controller('lessonTestsController', ["$scope", function($scope) {
+.controller('lessonsController', ["$scope", function($scope) {
 
+  
 
-
-}])  // end lessonTestsController
+}])  // end lessonsController
 
 angular.module('myApp')
 
-.directive('lessonTestsDirective', function() {
+.directive('lessonsSideBarDirective', ["$state", "$http", "$q", "lessonsContentService", function($state, $http, $q, lessonsContentService) {
 
   return {
-    restrict: 'A',
+    restrict: 'E',
+    controller: 'lessonsContentController',
+    templateUrl: './html/lessons/lessonsSideBarTemplate.html',
     link: function(scope, ele, attr) {
-      $('.reset-test').click(function() {
-        $('.final-score').css('display', 'none');
+      $('.lesson-title').click(function() {
+        let that = this;
+        
+        if ($state.name !== 'lessons') {
+          $state.go('lessons')
+        }
+        $('.lesson-sections', that.parentNode).toggle('expand');
+        $('.lesson-tests-wrapper').css('display', 'none');
       })
-      $('.lessons').click(function(){
-        $('.final-score').css('display', 'none');
-      })
-    }
+
+      let testNavigation = () => {
+        $('.lesson-tests-wrapper').css('display', 'block');
+        $('html, body').animate({ scrollTop: 0 }, 300);
+      }
+
+      $('.lesson-test').click(function() {
+        let lessonId = lessonsContentService.getTempId();
+        if ($state.name !== 'lessonTests') {
+          $state.go('lessonTests');
+          setTimeout(() => {
+            testNavigation();
+          }, 100);
+        } else {
+          testNavigation();
+        }
+      }) // end lesson-test click
+
+    } // end of directive link
   }
 
-})  // end lessonTestsDirective
-
+}])  // end lessonsSideBarDirective
 
 angular.module('myApp')
 .controller('loginController', ["$scope", "loginService", function($scope, loginService){
